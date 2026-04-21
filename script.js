62d3264b6d406c1e4ea395a8c49ce8d2d7f483b6
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-04-21 19:31:04 +0200

    feat : add  script.js file

---
+++ script.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+function addClick() {
+    count++;
+    document.getElementById("count").innerText = count;
+}
+
+function generatePassword() {
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+
+    for (let i = 0; i < 12; i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    document.getElementById("password").innerText = password;
+}
